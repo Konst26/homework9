4119de2889c432addd7f167bdb6380774ba1dc03
Константин <osokinkonstantin@mail.ru>
2024-05-14 21:38:57 +0300

    выполненные задачи

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        {
+            int[] pays = {1000, 1500, 2000, 2500, 1500};
+            int sum = 0;
+            for (int pay : pays) {
+                sum += pay;
+            }
+
+            System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+            System.out.println();
+        }
+        {
+            System.out.println("Задача 2");
+        }
+        int[] arr = {500, 1500, 2000, 2500, 1500, 1700, 1600};
+        int min = 10000;
+        int max = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей " + "Максимальная сумма трат за неделю составила " + max + " рублей");
+        System.out.println();
+        System.out.println("Задача 3");
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        double avg = sum / 4d;
+        System.out.println("Средняя сумма трат за месяц составила " + avg + " рублей");
+        System.out.println();
+        System.out.println("Задача 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i>=0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
